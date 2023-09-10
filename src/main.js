d84d1e4919b33b97fb567eaad3bba1db905fe748
Jakabos <jumamiller@yahoo.com>
2023-09-10 20:45:06 +0300

    auth plugin

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import './style.css'
 import App from './App.vue'
 import store from './store'
 import router from "./router/index.js";
-import listing from "./packages/Listing";//landing page
+import listing from "./packages/Listing";
+import Auth from "./packages/Auth/index.js";
+//landing page
 
 createApp(App)
+    .use(Auth,{router,store})
     .use(listing,{router,store})
     .use(router)
     .use(store)
