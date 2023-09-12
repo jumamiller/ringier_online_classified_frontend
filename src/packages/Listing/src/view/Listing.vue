dd891e25052c3beb9180adc61ca5911ddb71776d
Jakabos <jumamiller@yahoo.com>
2023-09-12 20:43:01 +0300

    change alt

--- src/packages/Listing/src/view/Listing.vue
+++ src/packages/Listing/src/view/Listing.vue
@@ -14,7 +14,7 @@
       <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         <div @click="$router.push({name:'PropertyDetailsCard',params:{code:property.slug}})" class="group relative"  v-bind:key="property.id"  v-for="property of properties">
           <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            <img :src="property?.images[0]?.image_path" alt="Front of men&#039;s Basic Tee in black." class="h-full w-full object-cover object-center lg:h-full lg:w-full">
+            <img :src="property?.images[0]?.image_path" alt="Property Image" class="h-full w-full object-cover object-center lg:h-full lg:w-full">
           </div>
           <div class="mt-4 flex justify-between">
             <div>
